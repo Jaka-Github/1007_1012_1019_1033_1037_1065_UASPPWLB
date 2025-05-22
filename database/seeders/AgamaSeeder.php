4eb48ff32ed0e8d456254fe38809b0645e9f7149
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-05-22 17:01:21 +0700

    firstorcreate agama seeder biar ga duplikat

--- database/seeders/AgamaSeeder.php
+++ database/seeders/AgamaSeeder.php
@@ -4,20 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Agama; // Pastikan model Agama sudah ada
 
 class AgamaSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('agama')->insert([
-            ['nama_agama' => 'Islam'],
-            ['nama_agama' => 'Kristen'],
-            ['nama_agama' => 'Hindu'],
-            ['nama_agama' => 'Budha'],
-            ['nama_agama' => 'Konghucu'],
-            ['nama_agama' => 'Katolik'],
-            ['nama_agama' => 'Kepercayaan'],
-        ]);
+        $agamas = [
+            'Islam',
+            'Kristen',
+            'Hindu',
+            'Budha',
+            'Konghucu',
+            'Katolik',
+            'Kepercayaan',
+        ];
+
+        foreach ($agamas as $nama) {
+            Agama::firstOrCreate(['nama_agama' => $nama]);
+        }
     }
 }
 
