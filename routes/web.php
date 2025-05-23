405e49f48890de3f035acfc3e1a63d554159237e
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-05-23 12:33:05 +0700

    komit pertama

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/dashboard', function () {
-        if (auth()->user()->role === 'admin') {
-            return redirect()->route('admin.statistik');
-        } else {
-            return view('users.dashboard');
-        }
-    })->name('dashboard');
+    Route::get('/user/dashboard', function () {
+        return view('users.dashboard');
+    })->name('users.dashboard');
+
+
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
