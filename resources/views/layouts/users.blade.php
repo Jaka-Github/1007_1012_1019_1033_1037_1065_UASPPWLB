ec530ae3c98a2a9cef54a76de01dc93c9b6cd5f9
aldntaretnas <aldinataretnas24@gmail.com>
2025-05-23 14:01:05 +0700

    Lihat anggota keluarga sementara

--- resources/views/layouts/users.blade.php
+++ resources/views/layouts/users.blade.php
@@ -62,7 +62,7 @@
 
 			<!-- Lihat Anggota Keluarga -->
 			<li class="rounded-sm hover:bg-indigo-100">
-				<a href="#" class="flex items-center p-2 space-x-3 rounded-md">
+				<a href="{{ route('anggota.index') }}" class="flex items-center p-2 space-x-3 rounded-md">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 fill-current text-indigo-600">
 					<path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/>
 					<circle cx="144" cy="208" r="16"/>
