093095c1f9604bfe1a2eb7e0514ca307f2aef135
arifachratu <arifachqueen@gmail.com>
2025-05-26 19:19:30 +0700

    perbaiki bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,7 +77,7 @@
                             Kelola Keluarga Pendikar
                         </a>
                         
-                        <a href="{{ route('diskusi.index') }}"> class="flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-lg hover:bg-indigo-50 hover:text-indigo-700 transition-colors duration-200">
+                        <a href="{{ route('diskusi.index') }}" class="flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-lg hover:bg-indigo-50 hover:text-indigo-700 transition-colors duration-200">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                             </svg>
