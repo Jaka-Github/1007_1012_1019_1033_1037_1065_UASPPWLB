35d496cf35d0e0a990bac986101e4b6d322043f1
Jaka Sampoerna <h1101231033@student.untan.ac.id>
2025-05-30 23:21:06 +0700

    styling form tambah anggota dan validasi

--- app/Http/Controllers/user/IbadahController.php
+++ app/Http/Controllers/user/IbadahController.php
@@ -27,8 +27,8 @@ class IbadahController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'category' => 'required|string',
-            'target' => 'nullable|string',
-            'duration' => 'nullable|string',
+            'target' => 'required|string',
+            'duration' => 'required|string',
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
         ]);
@@ -59,8 +59,8 @@ class IbadahController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'category' => 'required|string',
-            'target' => 'nullable|string',
-            'duration' => 'nullable|string',
+            'target' => 'required|string',
+            'duration' => 'required|string',
             'start_date' => 'required|date',
             'end_date' => 'required|date|after_or_equal:start_date',
         ]);
